c687225f68666b4061c72e63aa9503e9e7f1121d
kdcyberdude <kdsingh.cyberdude@gmail.com>
2019-07-13 16:29:42 +0530

    Front End

---
+++ src/js/app.js
@@ -0,0 +1,143 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  account: '0x0',
+  loading: false,
+  tokenPrice: 1000000000000000,
+  tokensSold: 0,
+  tokensAvailable: 750000,
+
+  init: function() {
+    console.log("App initialized...")
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
+    return App.initContracts();
+  },
+
+  initContracts: function() {
+    $.getJSON("KDSTokenSale.json", function(kdsTokenSale) {
+      App.contracts.KDSTokenSale = TruffleContract(kdsTokenSale);
+      App.contracts.KDSTokenSale.setProvider(App.web3Provider);
+      App.contracts.KDSTokenSale.deployed().then(function(kdsTokenSale) {
+        console.log("KDS Token Sale Address:", kdsTokenSale.address);
+      });
+    }).done(function() {
+      $.getJSON("KDSToken.json", function(kdsToken) {
+        App.contracts.KDSToken = TruffleContract(kdsToken);
+        App.contracts.KDSToken.setProvider(App.web3Provider);
+        App.contracts.KDSToken.deployed().then(function(kdsToken) {
+          console.log("KDS Token Address:", kdsToken.address);
+        });
+
+        App.listenForEvents();
+        return App.render();
+      });
+    })
+  },
+
+  // Listen for events emitted from the contract
+  listenForEvents: function() {
+    App.contracts.KDSTokenSale.deployed().then(function(instance) {
+      instance.Sell({}, {
+        fromBlock: 0,
+        toBlock: 'latest',
+      }).watch(function(error, event) {
+        console.log("event triggered", event);
+        App.render();
+
+      })
+    })
+  },
+
+  render: function() {
+    if (App.loading) {
+      return;
+    }
+    App.loading = true;
+
+    var loader  = $('#loader');
+    var content = $('#content');
+
+    loader.show();
+    content.hide();
+
+    // Load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if(err === null) {
+        App.account = account;
+        $('#accountAddress').html("Your Account: " + App.account);
+      }
+    })
+
+    // Load token sale contract
+    App.contracts.KDSTokenSale.deployed().then(function(instance) {
+      kdsTokenSaleInstance = instance;
+      return kdsTokenSaleInstance.tokenPrice();
+    }).then(function(tokenPrice) {
+      App.tokenPrice = tokenPrice;
+      $('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber());
+      return kdsTokenSaleInstance.tokensSold();
+    }).then(function(tokensSold) {
+      App.tokensSold = tokensSold.toNumber();
+      $('.tokens-sold').html(App.tokensSold);
+      $('.tokens-available').html(App.tokensAvailable);
+
+      var progressPercent = (Math.ceil(App.tokensSold) / App.tokensAvailable) * 100;
+      $('#progress').css('width', progressPercent + '%');
+
+      // Load token contract
+      App.contracts.KDSToken.deployed().then(function(instance) {
+        kdsTokenInstance = instance;
+        return kdsTokenInstance.balanceOf(App.account);
+      }).then(function(balance) {
+        $('.kds-balance').html(balance.toNumber());
+        App.loading = false;
+        loader.hide();
+        content.show();
+      })
+    });
+  },
+
+  buyTokens: function() {
+    $('#content').hide();
+    $('#loader').show();
+    var numberOfTokens = $('#numberOfTokens').val();
+    App.contracts.KDSTokenSale.deployed().then(function(instance) {
+      return instance.buyTokens(numberOfTokens, {
+        from: App.account,
+        value: numberOfTokens * App.tokenPrice,
+        gas: 500000 // Gas limit
+      });
+    }).then(function(result) {
+      console.log("Tokens bought...");
+      $('form').trigger('reset');
+      // reset number of tokens in form
+      // Wait for Sell event
+    }).catch(function(e){
+      console.log('error', e);
+      App.render();
+      if((String(e)).includes('from')){
+        alert('Try Again, because you changed the MetaMask account');
+      }else{
+        alert('You denied transaction');
+      }
+    });
+  }
+}
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  })
+});
